b1eb918fb630d9307cb27c49e3b40d5b3dbb3b10
Olteron <127535483+0lteron@users.noreply.github.com>
2024-03-23 11:22:28 +0100

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ def connect():
   conn = psycopg2.connect(
         dbname='', # Put the name of your database
         user='postgres', # Your username
-        cursor_factory=psycopg2.extras.NamedTupleCursor   # add the arguments password if you set up a password
+        cursor_factory=psycopg2.extras.NamedTupleCursor   # add the argument password if you set up a password
     )
   conn.autocommit = True
   return conn
